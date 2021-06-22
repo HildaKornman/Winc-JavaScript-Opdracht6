3eaf9fe9f61fdc959c794f359b4987d3a97e500d
HildaKornman <84178454+HildaKornman@users.noreply.github.com>
2021-06-22 22:24:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Opdracht 6
+const makePizza = function (){
+    console.log("Buy pizza ingredients");
+    console.log("Mix ingredients for dough");
+    console.log("Make pizza bottom");
+    console.log("Put toppings on it");
+    console.log("Put pizza in the oven for 12 minutes");
+};
+
+const makeSushi = function (){
+    console.log("Buy sushi ingredients");
+    console.log("Prepare sushi rice");
+    console.log("Prepare sushi tools");
+    console.log("Put rice on seaweed");
+    console.log("Put ingredients in sushi and roll");
+};
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
